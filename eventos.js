f97a8f13a85c08643b04a444c49105f9a005e126
lg <leonardoagustingimenez@gmail.com>
2023-08-21 20:25:42 -0300

    Al dar click en el botón ya no se disparaba el evento click del div

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 document.querySelector('div').addEventListener('click', () => {
     alert("Hola! Soy el div");
 });
+
+document.querySelector('button').addEventListener('click', (e) => {
+    e.stopPropagation();
+    alert("Hola!");
+});
